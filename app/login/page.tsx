e88b73c3322cf7b32a35df49c8160c393790e1f0
wazed biplob <www.biplob@gmail.com>
2024-11-03 23:00:50 +0600

    UI modification

---
+++ app/login/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+
+const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const router = useRouter();
+
+  const onSubmit = (data: any) => {
+    fetch("http://localhost:5000/api/v1/auth/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.success === true) {
+          if (data?.success === true) {
+            const email = data.data.email;
+            router.push(`/${email}`);
+          }
+        }
+      });
+  };
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="flex w-full flex-col md:w-1/2">
+          <div className="lg:w-[28rem] mx-auto my-auto flex flex-col justify-center pt-8 md:justify-start md:px-6 md:pt-0">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col pt-3 md:pt-8"
+            >
+              <div className="flex flex-col pt-4">
+                <div className="focus-within:border-b-gray-500 relative flex overflow-hidden border-b-2 transition">
+                  <input
+                    type="email"
+                    {...register("email", { required: true })}
+                    className="w-full flex-1 appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                    placeholder="Email"
+                    defaultValue="www.biplob@gmail.com"
+                  />
+                </div>
+              </div>
+              <div className="mb-12 flex flex-col pt-4">
+                <div className="focus-within:border-b-gray-500 relative flex overflow-hidden border-b-2 transition">
+                  <input
+                    type="password"
+                    {...register("password", { required: true })}
+                    className="w-full flex-1 appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                    placeholder="Password"
+                    defaultValue="123456"
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="w-full rounded-lg bg-gray-900 px-4 py-2 text-center text-base font-semibold text-white shadow-md ring-gray-500 ring-offset-2 transition focus:ring-2"
+              >
+                Log in
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="pointer-events-none relative hidden h-screen select-none bg-black md:block md:w-1/2">
+          <div className="absolute bottom-0 z-10 px-8 text-white opacity-100">
+            <p className="mb-8 text-3xl font-semibold leading-10">
+              We work 10x faster than our compeititors and stay consistant.
+              While they're bogged won with techincal debt, we're realeasing new
+              features.
+            </p>
+            <p className="mb-4 text-3xl font-semibold">Wazed Biplob</p>
+            <p className="mb-7 text-sm opacity-70">Link</p>
+          </div>
+          <img
+            className="-z-1 absolute top-0 h-full w-full object-cover opacity-90"
+            src="https://images.unsplash.com/photo-1565301660306-29e08751cc53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
